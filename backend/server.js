4f34702a5695c386b8a0cc123ba311dfd1b91510
Om Amonkar <ompamonkar@gmail.com>
2023-12-24 16:05:38 +0530

    body-parser bug fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
 const connectDB = require('./config/db.config')
+const bodyParser = require('body-parser')
 const testRouter = require('./routes/tests.routes')
 
 const app = express()
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 5000
 connectDB()
 
 app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
 
 app.use('/tests', testRouter)
 
